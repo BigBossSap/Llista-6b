24a3188867850a83360771afc951d52c81021a7b
Sergi Alesanco Perich <sergi.alesanco.perich@gmail.com>
2022-12-18 17:19:31 +0100

    add

---
+++ proves/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace proves
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto = "Línea 1\nLínea 2\nLínea 3";
+            Console.WriteLine(texto);
+        }
+    }
+}
